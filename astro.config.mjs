2c5d1c006a2b4a1683ab6ef8784344cffad43060
koala819 <x.genolhac@gmail.com>
2025-04-06 22:51:55 +0200

    fix: SEO with favicon and og-image

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,15 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import favicons from 'astro-favicons';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-
-  integrations: [react()]
+  site: 'https://landing-page-edu-x-com.vercel.app/',
+  base: '/',
+  trailingSlash: 'always',
+  integrations: [react(), favicons()],
 });
