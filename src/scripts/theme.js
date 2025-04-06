e5a43ad9463995c43e9947885e914287d01136ec
koala819 <x.genolhac@gmail.com>
2025-04-06 17:43:25 +0200

    fix: layouts

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,22 +1,23 @@
 // Fonction pour initialiser le thème
 function initTheme() {
-    // Vérifier les préférences sauvegardées ou le thème système
-    const isDark = localStorage.theme === 'dark' ||
-      (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
+  // Vérifier les préférences sauvegardées ou le thème système
+  const isDark =
+    localStorage.theme === 'dark' ||
+    (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
 
-    // Appliquer le thème
-    document.documentElement.classList.toggle('dark', isDark);
+  // Appliquer le thème
+  document.documentElement.classList.toggle('dark', isDark);
+}
+
+// Fonction pour changer de thème
+function toggleTheme() {
+  if (document.documentElement.classList.contains('dark')) {
+    document.documentElement.classList.remove('dark');
+    localStorage.theme = 'light';
+  } else {
+    document.documentElement.classList.add('dark');
+    localStorage.theme = 'dark';
   }
+}
 
-  // Fonction pour changer de thème
-  function toggleTheme() {
-    if (document.documentElement.classList.contains('dark')) {
-      document.documentElement.classList.remove('dark');
-      localStorage.theme = 'light';
-    } else {
-      document.documentElement.classList.add('dark');
-      localStorage.theme = 'dark';
-    }
-  }
-
-  export { initTheme, toggleTheme };
+export { initTheme, toggleTheme };
