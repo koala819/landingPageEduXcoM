8c28f984316e98cb8c5b4c4b0e7dfe1aa947076f
koala819 <x.genolhac@gmail.com>
2025-04-11 01:46:10 +0200

    maybe new

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,20 +1,18 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_CYrYMgcJ.mjs';
-import { manifest } from './manifest_CN9hIFZi.mjs';
+import { manifest } from './manifest_DO9siTGa.mjs';
 
 const serverIslandMap = new Map();;
 
 const _page0 = () => import('./pages/_image.astro.mjs');
-const _page1 = () => import('./pages/mentions-legales.astro.mjs');
-const _page2 = () => import('./pages/zoldcontact.astro.mjs');
-const _page3 = () => import('./pages/zoldindex.astro.mjs');
-const _page4 = () => import('./pages/index.astro.mjs');
+const _page1 = () => import('./pages/contact.astro.mjs');
+const _page2 = () => import('./pages/mentions-legales.astro.mjs');
+const _page3 = () => import('./pages/index.astro.mjs');
 const pageMap = new Map([
     ["node_modules/.pnpm/astro@5.6.1_@types+node@22.14.0_jiti@2.4.2_lightningcss@1.29.2_rollup@4.39.0_typescript@5.8.3/node_modules/astro/dist/assets/endpoint/generic.js", _page0],
-    ["src/pages/mentions-legales.astro", _page1],
-    ["src/pages/zOLDcontact.ts", _page2],
-    ["src/pages/zOLDindex.astro", _page3],
-    ["src/pages/index.astro", _page4]
+    ["src/pages/contact.ts", _page1],
+    ["src/pages/mentions-legales.astro", _page2],
+    ["src/pages/index.astro", _page3]
 ]);
 
 const _manifest = Object.assign(manifest, {
@@ -25,7 +23,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_astro-internal_middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "ec8c2fcd-e2ac-4955-9d2f-4fc88a387118",
+    "middlewareSecret": "5cba238e-17cd-4cfb-bb77-c02574506fe4",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
